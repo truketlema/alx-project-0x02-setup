0c8de1daa7407e39aaa60e59dcb50746db1e1de9
Truket Lema <truketlema9489@gmail.com>
2025-09-28 19:20:47 +0300

    done

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Link from "next/link"; // ✅ Make sure this line is present
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-600 text-white p-4">
+    <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">ALX Project 2</h1>
+      <nav>
+        {/* ✅ Navigation links */}
+        <Link href="/home" className="mr-4 hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+      </nav>
     </header>
   );
 };
