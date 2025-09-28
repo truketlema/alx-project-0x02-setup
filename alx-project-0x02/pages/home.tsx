e5fb01c98ccdf5c7abcd395a281a7c9143283800
Truket Lema <truketlema9489@gmail.com>
2025-09-28 19:26:33 +0300

    done

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Header from "../components/layout/Header";
-import Card from "../components/common/Card";
+import Card from "@/components/common/Card";
 
 const Home: NextPage = () => {
   return (
